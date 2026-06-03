889ffb2e85429dda94fe5f5207d9be755eedc25f
Anshu Kashyap <contactanshu247@gmail.com>
2026-06-03 16:08:31 +0530

    added methods

--- CLI_TO-DO App/main.go
+++ CLI_TO-DO App/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "os"
+import "fmt"
 
 func main() {
-	args := os.Args
+
+	todos := Todos{}
+	todos.add("Buy Milk")
+	todos.add("Buy bread")
+	todos.add("Buy toast")
+	fmt.Printf("%v\n", todos)
+	todos.delete(0)
+	fmt.Printf("%+v\n",todos) //+ will also add title,completed etc. in output
 	
 }
--- CLI_TO-DO App/models.go
+++ CLI_TO-DO App/models.go
@@ -1,10 +1,56 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Todo struct {
-	Title     string
-	Completed bool
-	CreatedAt time.Time
-	CompletedAt time.Time
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
 }
+
+type Todos []Todo //we are creating slice of Todo data structure to store multiple task in Todos.
+
+// Methods--->
+func (todos *Todos) add(title string) { //we are defining a method for Todos slice to add a Task.
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+		CompletedAt: nil,
+	}
+
+	*todos = append(*todos, todo) //appending the Task in Todos.
+
+}
+func (todos *Todos) validIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid Index")
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+	err := t.validIndex(index)
+	if err != nil {
+		return err
+	}
+
+	*todos = append(t[:index],t[index+1:]...)
+
+	return nil;
+
+}
+
+
+
+
+
