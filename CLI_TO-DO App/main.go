2c0103be245469a2c8d8a355445cfcb9efc879ad
Anshu Kashyap <contactanshu247@gmail.com>
2026-06-04 20:38:57 +0530

    features

--- CLI_TO-DO App/main.go
+++ CLI_TO-DO App/main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+
+
+// git status
+// git add .
+// git commit -m "Implement CLI todo app"
+// git push origin main
 
 func main() {
 
 	todos := Todos{}
 	todos.add("Buy Milk")
 	todos.add("Buy bread")
-	todos.add("Buy toast")
-	fmt.Printf("%v\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v\n",todos) //+ will also add title,completed etc. in output
+	//todos.add("Buy toast")
+	todos.print()
 	
+	//fmt.Printf("%+v\n",todos) //+ will also add title,completed etc. in output
+
 }
--- CLI_TO-DO App/models.go
+++ CLI_TO-DO App/models.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
@@ -16,6 +20,7 @@ type Todo struct {
 type Todos []Todo //we are creating slice of Todo data structure to store multiple task in Todos.
 
 // Methods--->
+
 func (todos *Todos) add(title string) { //we are defining a method for Todos slice to add a Task.
 	todo := Todo{
 		Title:       title,
@@ -27,6 +32,7 @@ func (todos *Todos) add(title string) { //we are defining a method for Todos sli
 	*todos = append(*todos, todo) //appending the Task in Todos.
 
 }
+
 func (todos *Todos) validIndex(index int) error {
 	if index < 0 || index >= len(*todos) {
 		err := errors.New("Invalid Index")
@@ -48,6 +54,56 @@ func (todos *Todos) delete(index int) error {
 
 	return nil;
 
+}
+
+func(todos *Todos) toggle(index int) error{
+	t := *todos
+
+	if err:= t.validIndex(index) ; err!=nil { //Shortcut to write Intialization and IF condition 
+		return err;
+	}
+
+	isCompleted := t[index].Completed //true or false
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime 
+	}
+
+	t[index].Completed=!isCompleted
+	 
+	return nil;
+
+}
+
+func(todos *Todos ) edit(index int , title string) error{
+	t:= *todos
+	if err := t.validIndex(index) ; err!=nil{
+		return err
+	}
+	t[index].Title = title
+
+	return nil
+}
+
+func(todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#","Title","Completed","Created At","Completed At")
+	for index,t := range *todos {
+		completed := "❌"
+		completedAt := ""
+		if t.Completed {
+			completed = "✅"
+			if t.CompletedAt!= nil{
+				completedAt= t.CompletedAt.Format(time.RFC1123)
+			}
+		}
+
+		table.AddRow(strconv.Itoa(index),t.Title,completed,t.CreatedAt.Format(time.RFC1123),completedAt)
+	}
+
+	table.Render()
 }
 
 
